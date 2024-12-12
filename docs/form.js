3595d87ee4efdf4d38e1fefdde7c46dc46af4e08
mayumiPc <mayumiobara089@gmail.com>
2024-12-12 22:56:29 +0900

    正規表現を用いて、不正なメールアドレスの書式に対してアラートを表示するコードを追加

--- docs/form.js
+++ docs/form.js
@@ -1,4 +1,17 @@
 // form.js
+function validateEmail(email) {
+    // メールアドレスの形式を判定する正規表現
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    // 正規表現で検証
+    if (!emailPattern.test(email)) {
+        // メールアドレスが正しくない場合アラートを表示
+        alert("正しいメールアドレスを入力してください。");
+        return false; // 検証失敗
+    }
+    return true; // 検証成功
+}
+
 document.getElementById("CONFIRM_BTN").addEventListener("click", () => {
     // 入力内容を取得
     const category = document.getElementById("category").value;
@@ -14,6 +27,8 @@ document.getElementById("CONFIRM_BTN").addEventListener("click", () => {
     if ((!email || email.length === 0) && (!message || message.length === 0)) {
         // 両方が未入力の場合にアラートを表示
         alert("メールアドレスとメッセージの両方を入力してください。");
+        return;
+    } else if (!validateEmail(email)) {
         return;
     }
     // 確認画面に移動
